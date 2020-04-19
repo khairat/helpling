e4ec021ef48b31e5731eb271ad0ee403d1291193
Ali Zahid <ali.zahid@live.com>
2020-04-19 08:37:15 +0400

    Bug fix

--- src/components/comments/comments.tsx
+++ src/components/comments/comments.tsx
@@ -61,7 +61,7 @@ const styles = StyleSheet.create({
   },
   list: {
     flexGrow: 1,
-    paddingVertical: layout.padding
+    paddingTop: layout.padding
   },
   main: {
     backgroundColor: colors.backgroundDark
